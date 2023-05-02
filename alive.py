a6c7608fe92c25b37187ad82c03a0022e9721fd7
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-05-02 09:35:39 +0630

    Update alive.py

--- alive.py
+++ alive.py
@@ -1,17 +1,4 @@
-from time import sleep
-from requests import get as rget
-from os import environ
-from logging import error as logerror, info as loginfo
+import requests
 
-BASE_URL = 'http://localhost'
-
-if BASE_URL is not None:
-    while True:
-        try:
-            rget(BASE_URL).status_code
-            loginfo("success")
-            sleep(6)
-        except Exception as e:
-            logerror(f"alive.py: {e}")
-            sleep(2)
-            continue
+response = requests.get('http://localhost')
+print(response.status_code)
