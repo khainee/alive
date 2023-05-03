d374741c2a760d0b074092233ad0668561a5b773
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-05-03 18:02:24 +0630

    Update alive.py

--- alive.py
+++ alive.py
@@ -8,8 +8,8 @@ BASE_URL = 'http://127.0.0.1'
 if BASE_URL is not None:
     while True:
         try:
-            rget(BASE_URL).status_code
-            loginfo("success")
+            res = rget(BASE_URL).status_code
+            loginfo(f"success {res}")
             sleep(6)
         except Exception as e:
             logerror(f"alive.py: {e}")
